4ef22d3e505c81f086ded0281ef7478f3771b72d
Javii <Javii@Javi>
2015-10-20 22:26:41 +0200

    Inicio

---
+++ Prog2-b5/src/prog2/b5/Prog2B5.java
@@ -0,0 +1,16 @@
+
+package prog2.b5;
+
+/**
+ *
+ * @author Javii
+ */
+public class Prog2B5 {
+
+    
+    public static void main(String[] args) {
+        Satelite satelite1 = new Satelite(5.20,20.30,30.10); 
+       satelite1.verPosicion();
+    }
+    
+}
